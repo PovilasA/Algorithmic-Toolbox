36b7f9026d9df9189759fe878079db5e4ba42dfb
Povilas Asijavičius <povilas.asijavicius@gmail.com>
2017-05-23 10:12:01 +0000

    More effective solution for max pairwise problem

--- max_pairwise_product.py
+++ max_pairwise_product.py
@@ -5,9 +5,19 @@ assert(len(a) == n)
 
 result = 0
 
-for i in range(0, n):
-    for j in range(i+1, n):
-        if a[i]*a[j] > result:
-            result = a[i]*a[j]
+# from IPython import embed
+# embed()
 
+max_index1 = 0 
+for i in range(1, n):
+    if a[i] > a[max_index1]:
+        max_index1 = i
+        
+max_index2 = False
+for i in range(0, n):
+    value = a[max_index2] if max_index2 else -1
+    if i != max_index1 and a[i] > value:
+        max_index2 = i
+
+result = a[max_index1] * a[max_index2]
 print(result)
